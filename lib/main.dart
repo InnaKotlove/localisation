eeeb7a078cdd0eb04967c4cce9b96343d17e769c
Inna Kotlovanovanova <Inna.Kotlovanova@gmail.com>
2022-04-12 16:37:49 +0300

    added new card

--- lib/main.dart
+++ lib/main.dart
@@ -56,6 +56,22 @@ class MyHomeWidget extends StatelessWidget {
 
   const MyHomeWidget({Key? key, required this.app}) : super(key: key);
 
+  Widget _buildLangButton(String lang) {
+    return  Padding(
+      padding: EdgeInsets.symmetric(vertical: 5.0),
+      child: MaterialButton(
+        color: app.loc.languageCode == lang ? Colors.green : Colors.white,
+        shape: const CircleBorder(),
+        onPressed: () {
+          app.changeLocale(lang);
+        },
+        child: Text(
+          lang.toUpperCase(),
+          style: TextStyle(color: Colors.black, fontSize: 20),
+        ),
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context) {
     var translation = AppLocalizations.of(context)!;
@@ -63,64 +79,83 @@ class MyHomeWidget extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         actions: <Widget>[
-          MaterialButton(
-            color: Colors.green,
-            shape: const CircleBorder(),
-            onPressed: () {
-              app.changeLocale('en');
-            },
-            child: Text(
-              'En',
-              style: TextStyle(color: Colors.white, fontSize: 20),
-            ),
-          ),
-          MaterialButton(
-            color: Colors.green,
-            shape: const CircleBorder(),
-            onPressed: () {
-              app.changeLocale('ru');
-            },
-            child: Text(
-              'Ru',
-              style: TextStyle(color: Colors.white, fontSize: 20),
-            ),
-          ),
+          _buildLangButton('en'),
+          _buildLangButton('ru'),
         ],
         title: Text(translation.title),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          Card(
-            elevation: 5,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    height: 150,
-                    width: 150,
-                    child: Image.network(
-                        'https://i.pinimg.com/originals/a1/90/5c/a1905c3d2adac96c9e9c094dccafc857.jpg'),
-                  ),
-                  SizedBox(width: 12),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        translation.winnieName,
-                        style: TextStyle(fontSize: 20),
-                      ),
-                      Text('Jan 17, 2022'),
-                      Text('4 Jars - USD20'),
-                    ],
-                  ),
-                ],
-              ),
-            ),
+          MyCard(
+            image: Image.network(
+                'https://i.pinimg.com/originals/a1/90/5c/a1905c3d2adac96c9e9c094dccafc857.jpg'),
+            title: translation.winnieName,
+            data: translation.born(DateTime(2022, DateTime.january, 17)),
+            price: translation.honey(4) + " " + translation.price(20.0),
+          ),
+          MyCard(
+            image: Image.network(
+                'https://static.wikia.nocookie.net/disney/images/d/da/Rabbit_Winnie_the_Pooh.jpg/revision/latest/top-crop/width/360/height/450?cb=20170928185405'),
+            title: translation.rabbit,
+            data: translation.born(DateTime(2021, DateTime.april, 19)),
+            price: translation.honey(1) + " " + translation.price(20.0),
+          ),
+          MyCard(
+            image: Image.network(
+                'https://sidex.ru/images_offers/330/330744/nakleiki_roommates_rmk1500gm_disney_vinni_pyh_2.jpg'),
+            title: translation.tiger,
+            data: translation.born(DateTime(2021, DateTime.february, 07)),
+            price: translation.honey(2) + " " + translation.price(5.0),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class MyCard extends StatelessWidget {
+  final Image image;
+  final String title;
+  final String data;
+  final String price;
+
+  const MyCard(
+      {Key? key,
+      required this.image,
+      required this.title,
+      required this.data,
+      required this.price})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 150,
+              width: 150,
+              child: image,
+            ),
+            SizedBox(width: 12),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: TextStyle(fontSize: 20),
+                ),
+                Text(data),
+                Text(price),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
